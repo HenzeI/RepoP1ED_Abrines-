0bcc1c959541a890d7aed10db3195cec565e69a4
DAM102 <DAM102@DESKTOP-NJHVLE7.iesataulfo.local>
2023-03-20 19:07:19 +0100

    Segunda version

--- Proyecto_1/src/paquete1/Principal.java
+++ Proyecto_1/src/paquete1/Principal.java
@@ -3,6 +3,6 @@ package paquete1;
 public class Principal {
 
 	public static void main(String[] args) {
-		System.out.println("Programa que nos permitira hacer un control de versiones con Git");
+		System.out.println("Programa que nos permitira hacer un control de versiones con Git Prueba");
 	}
 }
